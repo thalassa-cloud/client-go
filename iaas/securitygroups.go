426f1e9342b85687829faf35466a91fe92dce47b
Thomas Kooi <thomas@thalassa.cloud>
2025-05-23 20:51:39 +0200

    refactor(iaas): improve security group types

--- iaas/securitygroups.go
+++ iaas/securitygroups.go
@@ -15,29 +15,29 @@ const (
 )
 
 type CreateSecurityGroupRequest struct {
-	// Name is the name of the security group
-	Name string `json:"name" validate:"required,min=1,max=16,notblank,ascii,is-trimmed"`
+	// Name is the name of the security group. Must be between 1 and 16 characters and contain only ASCII characters.
+	Name string `json:"name"`
 	// Description is the description of the security group
-	Description string `json:"description" validate:"omitempty,max=255"`
+	Description string `json:"description"`
 	// Labels are the labels of the security group
 	Labels Labels `json:"labels"`
 	// Annotations are the annotations of the security group
 	Annotations Annotations `json:"annotations"`
 	// VpcIdentity is the identity of the VPC that the security group belongs to
-	VpcIdentity string `json:"vpcIdentity" validate:"required"`
+	VpcIdentity string `json:"vpcIdentity"`
 	// AllowSameGroupTraffic is a flag that indicates if the security group allows traffic between instances in the same security group
 	AllowSameGroupTraffic bool `json:"allowSameGroupTraffic"`
 	// IngressRules are the ingress rules of the security group
-	IngressRules []SecurityGroupRule `json:"ingressRules" validate:"omitempty,dive"`
+	IngressRules []SecurityGroupRule `json:"ingressRules"`
 	// EgressRules are the egress rules of the security group
-	EgressRules []SecurityGroupRule `json:"egressRules" validate:"omitempty,dive"`
+	EgressRules []SecurityGroupRule `json:"egressRules"`
 }
 
 type UpdateSecurityGroupRequest struct {
 	// Name is the name of the security group
-	Name string `json:"name" validate:"min=1,max=16,notblank,ascii,is-trimmed"`
+	Name string `json:"name"`
 	// Description is the description of the security group
-	Description string `json:"description" validate:"omitempty,max=255"`
+	Description string `json:"description"`
 	// Labels are the labels of the security group
 	Labels Labels `json:"labels"`
 	// Annotations are the annotations of the security group
@@ -47,9 +47,9 @@ type UpdateSecurityGroupRequest struct {
 	// AllowSameGroupTraffic is a flag that indicates if the security group allows traffic between instances in the same security group
 	AllowSameGroupTraffic bool `json:"allowSameGroupTraffic"`
 	// IngressRules are the ingress rules of the security group
-	IngressRules []SecurityGroupRule `json:"ingressRules" validate:"omitempty,dive"`
+	IngressRules []SecurityGroupRule `json:"ingressRules"`
 	// EgressRules are the egress rules of the security group
-	EgressRules []SecurityGroupRule `json:"egressRules" validate:"omitempty,dive"`
+	EgressRules []SecurityGroupRule `json:"egressRules"`
 }
 
 type SecurityGroupStatus string
@@ -73,8 +73,6 @@ type SecurityGroup struct {
 
 	Labels      Labels      `json:"labels"`
 	Annotations Annotations `json:"annotations"`
-
-	DeletedAt *time.Time `json:"deletedAt,omitempty"`
 	// Organisation is the organisation that the security group belongs to
 	Organisation *base.Organisation `json:"organisation,omitempty"`
 	// Vpc is the VPC that the security group belongs to
@@ -83,34 +81,34 @@ type SecurityGroup struct {
 	// Status is the status of the security group
 	Status SecurityGroupStatus `json:"status,omitempty"`
 	// AllowSameGroupTraffic is a flag that indicates if the security group allows traffic between instances in the same security group
-	AllowSameGroupTraffic bool `json:"allowSameGroupTraffic" validate:"required"`
+	AllowSameGroupTraffic bool `json:"allowSameGroupTraffic"`
 	// IngressRules are the ingress rules of the security group
-	IngressRules []SecurityGroupRule `json:"ingressRules" validate:"required,dive,max=60,min=0"`
+	IngressRules []SecurityGroupRule `json:"ingressRules"`
 	// EgressRules are the egress rules of the security group
-	EgressRules []SecurityGroupRule `json:"egressRules" validate:"required,dive,max=60,min=0"`
+	EgressRules []SecurityGroupRule `json:"egressRules"`
 }
 
 type SecurityGroupRule struct {
 	// Name is the name of the rule
-	Name string `json:"name" validate:"omitempty,max=255"`
+	Name string `json:"name"`
 	// IPVersion is the IP version of the rule
-	IPVersion SecurityGroupIPVersion `json:"ipVersion" validate:"required"`
+	IPVersion SecurityGroupIPVersion `json:"ipVersion"`
 	// Protocol is the protocol of the rule
-	Protocol SecurityGroupRuleProtocol `json:"protocol" validate:"required"`
-	// Priority is the priority of the rule
-	Priority int32 `json:"priority" validate:"required,gte=1,lte=200"`
+	Protocol SecurityGroupRuleProtocol `json:"protocol"`
+	// Priority is the priority of the rule. Must be greater than 0 and less than 200.
+	Priority int32 `json:"priority"`
 	// RemoteType is the type of the remote address
-	RemoteType SecurityGroupRuleRemoteType `json:"remoteType" validate:"required"`
+	RemoteType SecurityGroupRuleRemoteType `json:"remoteType"`
 	// RemoteAddress is the IP address or CIDR block that the rule applies to
-	RemoteAddress *string `json:"remoteAddress" validate:"omitempty"`
+	RemoteAddress *string `json:"remoteAddress"`
 	// RemoteSecurityGroupIdentity is the identity of the security group that the rule applies to
-	RemoteSecurityGroupIdentity *string `json:"remoteSecurityGroupIdentity" validate:"omitempty"`
-	// PortRangeMin is the minimum port of the rule
-	PortRangeMin int32 `json:"portRangeMin" validate:"omitempty,gte=1,lte=65535"`
-	// PortRangeMax is the maximum port of the rule
-	PortRangeMax int32 `json:"portRangeMax" validate:"omitempty,gte=0,lte=65535"`
+	RemoteSecurityGroupIdentity *string `json:"remoteSecurityGroupIdentity"`
+	// PortRangeMin is the minimum port of the rule. Must be greater than 0 and less than 65535.
+	PortRangeMin int32 `json:"portRangeMin"`
+	// PortRangeMax is the maximum port of the rule. Must be greater than 0 and less than 65535.
+	PortRangeMax int32 `json:"portRangeMax"`
 	// Policy is the policy of the rule
-	Policy SecurityGroupRulePolicy `json:"policy" validate:"required"`
+	Policy SecurityGroupRulePolicy `json:"policy"`
 }
 
 type SecurityGroupRuleProtocol string
