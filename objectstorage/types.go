3dd84432bd3ecfe1638a91b9040b72695c88bc65
Thomas Kooi <thomas@thalassa.cloud>
2025-09-18 19:16:44 +0200

    feat(objectstorage): expand bucket types with versioning and object lock features

--- objectstorage/types.go
+++ objectstorage/types.go
@@ -1,10 +1,19 @@
 package objectstorage
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/thalassa-cloud/client-go/iaas"
 	"github.com/thalassa-cloud/client-go/pkg/base"
+)
+
+type ObjectStorageBucketVersioning string
+
+const (
+	ObjectStorageBucketVersioningDisabled  ObjectStorageBucketVersioning = "Disabled"
+	ObjectStorageBucketVersioningEnabled   ObjectStorageBucketVersioning = "Enabled"
+	ObjectStorageBucketVersioningSuspended ObjectStorageBucketVersioning = "Suspended"
 )
 
 type ObjectStorageBucket struct {
@@ -12,6 +21,9 @@ type ObjectStorageBucket struct {
 	Organisation *base.Organisation `json:"organisation,omitempty"`
 	CreatedAt    time.Time          `json:"createdAt"`
 	UpdatedAt    time.Time          `json:"updatedAt"`
+
+	Labels      map[string]string `json:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
 
 	// Name is the name of the bucket
 	Name string `json:"name"`
@@ -21,19 +33,21 @@ type ObjectStorageBucket struct {
 	Public bool `json:"public"`
 	// Status is the status of the bucket
 	Status string `json:"status"`
-	// Usage is the usage of the bucket
-	Usage Usage `json:"usage"`
-
-	// Endpoint for the bucket, is collected from the CR
+	// Endpoint for the bucket
 	Endpoint string `json:"endpoint"`
-
+	// Usage is the usage of the bucket
+	Usage ObjectStorageBucketUsage `json:"usage" bun:"usage,type:jsonb"`
+	// Versioning is the versioning of the bucket
+	Versioning ObjectStorageBucketVersioning `json:"versioning" bun:"versioning"`
+	// ObjectLockEnabled is the object lock of the bucket
+	ObjectLockEnabled bool `json:"objectLockEnabled" bun:"object_lock_enabled"`
 	// Region is the region of the bucket
 	Region *iaas.Region `json:"cloudRegion,omitempty"`
 }
 
-type Usage struct {
-	TotalSizeGB  int `json:"total_size_gb"`
-	TotalObjects int `json:"total_objects"`
+type ObjectStorageBucketUsage struct {
+	TotalSizeGB  float64 `json:"total_size_gb"`
+	TotalObjects int64   `json:"total_objects"`
 }
 
 type CreateBucketRequest struct {
@@ -46,14 +60,28 @@ type CreateBucketRequest struct {
 	Region string `json:"region"`
 	// PolicyDocument is the policy document for the bucket.
 	PolicyDocument *PolicyDocument `json:"policy,omitempty"`
+	// Labels is the labels of the bucket.
+	Labels map[string]string `json:"labels,omitempty"`
+	// Annotations is the annotations of the bucket.
+	Annotations map[string]string `json:"annotations,omitempty"`
+	// Versioning is the versioning of the bucket.
+	Versioning ObjectStorageBucketVersioning `json:"versioning"`
+	// ObjectLockEnabled is the object lock enabled of the bucket.
+	ObjectLockEnabled bool `json:"objectLockEnabled"`
 }
 
 type UpdateBucketRequest struct {
 	// Public is a flag that indicates if the bucket can be accessed by the public.
 	Public bool `json:"public"`
-
 	// PolicyDocument is the policy document for the bucket.
 	PolicyDocument *PolicyDocument `json:"policy,omitempty"`
+	// Versioning is the versioning of the bucket.
+	Versioning ObjectStorageBucketVersioning `json:"versioning"`
+	// ObjectLockEnabled is the object lock enabled of the bucket.
+	ObjectLockEnabled *bool `json:"objectLockEnabled"`
+
+	Labels      map[string]string `json:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
 }
 
 // PolicyDocument represents a full S3 bucket policy.
@@ -74,5 +102,15 @@ type Statement struct {
 
 // Principal defines which user(s) the statement applies to.
 type Principal struct {
-	AWS interface{} `json:"AWS"` // can be string or []string
+	AWS      interface{} `json:"AWS"`      // can be string or []string
+	Thalassa interface{} `json:"Thalassa"` // can be string or []string
+}
+
+type PrincipalARN string
+
+func (p PrincipalARN) Validate() error {
+	if p == "" {
+		return fmt.Errorf("principal ARN is required")
+	}
+	return nil
 }
